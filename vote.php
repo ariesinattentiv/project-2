0c7a45d3905ec42f0ed151132d081f78b418cdcb
schrmad <104103941+schrmad@users.noreply.github.com>
2022-11-04 23:03:10 -0400

    voting works

--- vote.php
+++ vote.php
@@ -1,17 +1,17 @@
 <?php 
 session_start();
 require_once("dbconnections.php");
-
+// With assistance from Caroline Lamb
 try{
-    if(isset($_POST['submit'])){
+    if(!empty($_GET['points']) && !empty($_GET['id'])){
         $db->beginTransaction(); 
-        $statement = $db->prepare("INSERT INTO `travelimagerating` (ImageID, Rating) VALUES (1, 2)"); 
+        $statement = $db->prepare("INSERT INTO travelimagerating (ImageID, Rating) VALUES (?, ?)"); 
         $statement->bindValue(1, $_GET['id']); 
         $statement->bindValue(2, $_GET['points']); 
         $statement->execute(); 
         $db->commit();
       }
-    header("Location: image.php?id=".$_GET['id']);
+    header("Location: http://localhost/ga2/image.php?id=".$_GET['id']);
 }
 catch(PDOException $e) {
     header("Location: error.php?error=Connection failed: ". $e->getMessage());
